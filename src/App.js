14d833b41fbf94986dc111182b9d5f41969cd6c0
vinaybajjuri58 <vinaybajjuri58@gmail.com>
2021-09-28 02:40:29 +0530

    resolved deploy errors

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
 import * as React from "react";
-import { ethers } from "ethers";
-import './App.css';
+// import { ethers } from "ethers";
+import "./App.css";
 
 export default function App() {
+  const wave = () => {};
 
-  const wave = () => {
-    
-  }
-  
   return (
     <div className="mainContainer">
-
       <div className="dataContainer">
         <div className="header">
-        👋 Hey there!
+          <span role="img" aria-label="wave">
+            👋
+          </span>{" "}
+          Hey there!
         </div>
 
         <div className="bio">
-        I am farza and I worked on self-driving cars so that's pretty cool right? Connect your Ethereum wallet and wave at me!
+          I am farza and I worked on self-driving cars so that's pretty cool
+          right? Connect your Ethereum wallet and wave at me!
         </div>
 
         <button className="waveButton" onClick={wave}>
